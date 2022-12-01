50df791c9c7961c85b87bdccebe5c509a54b3869
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2022-12-01 15:12:41 +0900

    feat: 회원가입 페이지 라우터 추가
    feat: 회원가입 페이지 라우터 추가

---
+++ src/components/SignUp/SignUp.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { toast, ToastContainer } from 'react-toastify';
+
+function SignUp() {
+  
+  return (
+    <>
+      <div>
+        안녕
+      </div>
+    </>
+  );
+}
+
+export default SignUp;
---
+++ src/components/SignUp/SignUp.styles.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 100%;
+  padding: 2rem 1.5rem 2rem 1.5rem;
+`;
+
+export { Wrapper, };
