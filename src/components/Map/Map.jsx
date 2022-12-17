463f5016d87b3df72d27ef828ed57e5c1e1b8fd0
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2022-12-17 15:57:31 +0900

    feat: 마커 현재 위치 연동 구현
    feat: 마커 현재 위치 연동 구현

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,40 +1,54 @@
 /* global kakao */
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import cn from "classnames";
 import { Wrapper, MapContainer, } from "./Map.styles";
 
 const { kakao } = window;
 
+// 지도 카카오맵
 function Map() {
+  
   useEffect(() => {
+    // 지도
     const container = document.getElementById("map");
-
     const options = {
       center: new window.kakao.maps.LatLng(37.52197524055062, 127.16017523675508),
       level: 8,
     };
-
-    // 지도
     const map = new window.kakao.maps.Map(container, options);
-
-    // 지도 로딩 시
     console.log("loading kakaomap");
 
-    // 마커
-    /* eslint-disable */
-    let markerPosition = new window.kakao.maps.LatLng(37.52197524055062, 127.16017523675508);
+    // 현 위치 확인
+    let lat = 37.4812845080678;
+    let lon = 126.952713197762;
 
-    // 마커 생성
-    let marker = new window.kakao.maps.Marker({
-      position: markerPosition,
-    });
-    /* eslint-enable */
-
-    // 마커 지도에 표시
-    marker.setMap(map);
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        lat = position.coords.latitude; // 위도
+        lon = position.coords.longitude; // 경도
+        /* eslint-disable */
+        let marker = new window.kakao.maps.Marker({
+          position: new window.kakao.maps.LatLng(lat, lon),
+        });
+        /* eslint-enable */
+        marker.setMap(map);
+        map.setCenter(new window.kakao.maps.LatLng(lat, lon));
+        console.log('현재 위치:', lat, lon);
+      });
+    } 
+    else {
+      /* eslint-disable */
+      let marker = new window.kakao.maps.Marker({
+        position: new window.kakao.maps.LatLng(lat, lon),
+      });
+      /* eslint-enable */
+      marker.setMap(map);
+      map.setCenter(new window.kakao.maps.LatLng(lat, lon));
+      console.log('기본 위치:', lat, lon)
+    };
 
   }, []);
-
+  
   return (
     <>
       <Wrapper>
