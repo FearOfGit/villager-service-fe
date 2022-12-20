fd07905c80423dd0cde515f52d33eaca6cafbd0b
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2022-12-20 20:08:59 +0900

    fix: 동네 목록 조회 API 통신
    fix: 동네 목록 조회 API 통신

--- src/components/Map/AddMap.jsx
+++ src/components/Map/AddMap.jsx
@@ -25,40 +25,46 @@ function AddMap () {
         toast.error(e.response.data.errorMessage);
       });
   }
-  
-  function getTownList () {
+
+  const [ town, setTown ] = useState();
+  const [village, setVillage] = useState('아무개동');
+
+  useEffect(() => {
     const body = {
       latitude: location.value.latitude,
       longitude: location.value.longitude
     };
-    return searchTownAPI(body).then((res) => res.data);
-  }
-  const { data } = useQuery('getTown', getTownList);
-  console.log('되냐', data);
-  console.log("변화된 위치", location.value.latitude, location.value.longitude);
-  console.log("불러온 데이터", data);
+    searchTownAPI(body).then((res) => {
+      // console.log(body, res.data);
+      setTown(res.data.towns);
+      setVillage(town[0].name.split(' ')[2]);
+    });
+  }, [town]);
+  
   return (
     <>
     <ToastContainer/>
-    <Wrapper>
-      <SubmitWrapper>
-        <ContentSection>
-          현 위치에 기반한 회원님의 동네는
-          <br/>
-          !연남동!
-          &nbsp;
-          입니다.
-        </ContentSection>
-        <ButtonSection>
-          <Button type="button" onClick={()=>addTown()}>
-            동네 설정하기
-          </Button>
-          <Button type="button" onClick={()=>getTownList()}>
-            동네 목록 조회하기
-          </Button>
-        </ButtonSection>
-      </SubmitWrapper>
-    </Wrapper>
+    {town && (
+      <Wrapper>
+        <SubmitWrapper>
+          <ContentSection>
+            현 위치에 기반한 회원님의 동네는
+            <br/>
+            {village}
+            &nbsp;
+            입니다.
+          </ContentSection>
+          <ButtonSection>
+            <Button type="button" onClick={()=>addTown()}>
+              동네 설정하기
+            </Button>
+            <Button type="button">
+              동네 목록 조회하기
+            </Button>
+          </ButtonSection>
+        </SubmitWrapper>
+      </Wrapper>
+    )}
     </>
   );
 }
