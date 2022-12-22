b31caba30a0132735d48312561eab212e9b01d08
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2022-12-23 02:55:34 +0900

    fix: API 관련 BASE URL 수정
    fix: API 관련 BASE URL 수정

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useQuery } from 'react-query';
 import { Wrapper, } from './Board.styles';
+import { postsListAPI} from '../../api/Board';
 
 function Board () {
+
+  const [ categoryId1, setCategoryId ] = useState(1);
+  const [ page1, setPage ] = useState(0);
+  const [ size1, setSize ] = useState(5);
+  
+  function getBoard () {
+    /* eslint-disable-next-line */
+    const params = {params: {categoryId: categoryId1, page: page1, size: size1}};
+    return postsListAPI(params).then((res)=>res.data);
+  }
+  const { data } = useQuery('getBoard', getBoard);
+  console.log(data);
+
   return(
     <>
       <Wrapper>
-
+        안녕
       </Wrapper>
     </>
   );
