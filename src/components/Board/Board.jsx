3419b4eea971f0293c36f749a980748fec3a1d2b
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2022-12-23 16:29:04 +0900

    feat: 게시판 구현 중
    feat: 게시판 구현 중

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,26 +1,46 @@
 import React, { useState } from 'react';
 import { useQuery } from 'react-query';
-import { Wrapper, } from './Board.styles';
+import { Wrapper, Title, } from './Board.styles';
 import { postsListAPI} from '../../api/Board';
+import Card from './Card';
 
 function Board () {
 
-  const [ categoryId1, setCategoryId ] = useState(1);
-  const [ page1, setPage ] = useState(0);
-  const [ size1, setSize ] = useState(5);
+  const [ boardCategoryId, setBoardCategoryId ] = useState(1);
+  const [ boardPage, setBoardPage ] = useState(0);
+  const [ boardSize, setBoardSize ] = useState(5);
   
   function getBoard () {
-    /* eslint-disable-next-line */
-    const params = {params: {categoryId: categoryId1, page: page1, size: size1}};
+    const params = {params: {categoryId: boardCategoryId, page: boardPage, size: boardSize}};
     return postsListAPI(params).then((res)=>res.data);
   }
   const { data } = useQuery('getBoard', getBoard);
   console.log(data);
 
+  let  boardList = [];
+
+  if (data) {
+    boardList = data.content;
+    console.log(boardList);
+  };
+
+
   return(
     <>
       <Wrapper>
-        안녕
+        <Title>
+          게시판
+        </Title>
+        {boardList.map((post)=>(
+          <Card
+            key={post.postId}
+            postId={post.postId}
+            title={post.title}
+            nickName={post.nickName}
+            nearCreateAt={post.nearCreateAt}
+            categoryId={post.categoryId}
+          />
+        ))}
       </Wrapper>
     </>
   );
